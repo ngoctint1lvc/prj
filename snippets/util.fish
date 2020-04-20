f7ab04ae0acddedd2dd8249b646883deaa8cd4f1
tran ngoc tin <ngoctint1lvc@gmail.com>
2020-04-20 20:50:42 +0700

    fix bug && symbol for fish shell

--- snippets/util.fish
+++ snippets/util.fish
@@ -7,13 +7,15 @@ function ntfind
 
     set -l CURRENT_DIR (dirname (status -f))
     set -l project_finder_tool (readlink -f $CURRENT_DIR/../project-finder.py)
-    echo -n ($python_bin $project_finder_tool $argv 3>&1 1>(tty) 2>/dev/null)
+    echo -n (eval "$python_bin $project_finder_tool $argv 3>&1 1>(tty) 2>/dev/null")
 end
 
 # only work if you installed vscode
 function ntcode
     set -l dir (ntfind $argv);
-    [ -e "$dir" ] && code $dir
+    if test -e "$dir"
+        code $dir
+    end
 end
 
 function prj
